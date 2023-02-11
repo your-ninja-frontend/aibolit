878342b3ae6b1b09b911381df9859a9068b74748
ninja_frontend <stifffffler102@gmail.com>
2023-02-11 23:58:25 +0300

    new file:   build/img/place.svg new file:   build/img/tel.svg new file:   build/img/work-time.svg modified:   build/index.html new file:   source/js/map.js new file:   source/js/mock.js new file:   source/sass/contacts.scss new file:   source/sass/footer.scss modified:   source/sass/global.scss modified:   source/sass/header.scss modified:   source/sass/main.scss new file:   source/sass/map.scss modified:   source/sass/social.scss

---
+++ source/js/map.js
@@ -0,0 +1,59 @@
+const map = L.map('map')
+  .setView({
+    lat: 48.76448725577362,
+    lng: 44.478829,
+  }, 11);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
+
+const points = [
+  {
+    title: 'Айболит на Краснополянской',
+    description: `Дзержинский район,
+    ул. Краснополянская, 30`,
+    tel: '962292',
+    workTime: 'Круглосуточно',
+    lat: 48.764467,
+    lng: 44.478860,
+  },
+  {
+    title: 'Айболит на Тулака',
+    description: `Советский район,
+    ул. Карла Маркса, 7`,
+    tel: '962292',
+    workTime: 'Круглосуточно',
+    lat: 48.676486,
+    lng: 44.478725,
+  },
+];
+
+const createPopup = ({ title, description, workTime, tel}) => `
+  <section class="map-baloon">
+  <h3 class="map-baloon__title">${title}</h3>
+  <p class="map-baloon__adress">${description}</p>
+  <a class="map-baloon__tel" href="tel:+${tel}">${tel}</a>
+  <p class="map-baloon__work-time">${workTime}</p>
+  </section>
+`;
+
+points.forEach((point) => {
+  const {lat, lng} = point;
+
+  const marker = L.marker(
+    {
+      lat,
+      lng
+    },
+  );
+
+  marker.addTo(map).bindPopup(createPopup(point),
+  {
+    keepInView: true,
+  },
+  );
+});
