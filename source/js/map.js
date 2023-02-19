cb62a320ebdba4164e01cadde345dbb4079075f0
ninja_frontend <stifffffler102@gmail.com>
2023-02-19 21:15:57 +0300

    new file:   build/img/back-link-arrow.svg new file:   build/img/therapy-blog-1.jpg modified:   build/index.html new file:   build/services.html modified:   source/js/map.js modified:   source/js/slick-slider.js modified:   source/sass/about-company.scss modified:   source/sass/achievements.scss modified:   source/sass/advantages.scss new file:   source/sass/all-services.scss new file:   source/sass/appointments.scss modified:   source/sass/change-city.scss modified:   source/sass/colors.scss modified:   source/sass/contacts.scss modified:   source/sass/footer.scss modified:   source/sass/global.scss modified:   source/sass/header.scss new file:   source/sass/link-back.scss modified:   source/sass/main.scss modified:   source/sass/menu.scss modified:   source/sass/services.scss modified:   source/sass/slick.scss modified:   source/sass/slider.scss modified:   source/sass/social.scss new file:   source/sass/therapy-main-content.scss new file:   source/sass/therapy-price.scss new file:   source/sass/therapy.scss modified:   source/sass/variables.scss

--- source/js/map.js
+++ source/js/map.js
@@ -1,38 +1,39 @@
-const map = L.map('map')
-  .setView({
-    lat: 48.76448725577362,
-    lng: 44.478829,
-  }, 11);
+if (document.querySelector('.map')) {
+  const map = L.map('map')
+    .setView({
+      lat: 48.76448725577362,
+      lng: 44.478829,
+    }, 11);
 
-L.tileLayer(
-  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-  {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  },
-).addTo(map);
+  L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    },
+  ).addTo(map);
 
-const points = [
-  {
-    title: 'Айболит на Краснополянской',
-    description: `Дзержинский район,
+  const points = [
+    {
+      title: 'Айболит на Краснополянской',
+      description: `Дзержинский район,
     ул. Краснополянская, 30`,
-    tel: '962292',
-    workTime: 'Круглосуточно',
-    lat: 48.764467,
-    lng: 44.478860,
-  },
-  {
-    title: 'Айболит на Тулака',
-    description: `Советский район,
+      tel: '962292',
+      workTime: 'Круглосуточно',
+      lat: 48.764467,
+      lng: 44.478860,
+    },
+    {
+      title: 'Айболит на Тулака',
+      description: `Советский район,
     ул. Карла Маркса, 7`,
-    tel: '962292',
-    workTime: 'Круглосуточно',
-    lat: 48.676486,
-    lng: 44.478725,
-  },
-];
+      tel: '962292',
+      workTime: 'Круглосуточно',
+      lat: 48.676486,
+      lng: 44.478725,
+    },
+  ];
 
-const createPopup = ({ title, description, workTime, tel}) => `
+  const createPopup = ({ title, description, workTime, tel }) => `
   <section class="map-baloon">
   <h3 class="map-baloon__title">${title}</h3>
   <p class="map-baloon__adress">${description}</p>
@@ -41,19 +42,22 @@ const createPopup = ({ title, description, workTime, tel}) => `
   </section>
 `;
 
-points.forEach((point) => {
-  const {lat, lng} = point;
+  points.forEach((point) => {
+    const { lat, lng } = point;
 
-  const marker = L.marker(
-    {
-      lat,
-      lng
-    },
-  );
+    const marker = L.marker(
+      {
+        lat,
+        lng
+      },
+    );
 
-  marker.addTo(map).bindPopup(createPopup(point),
-  {
-    keepInView: true,
-  },
-  );
-});
+    marker.addTo(map).bindPopup(createPopup(point),
+      {
+        keepInView: true,
+      },
+    );
+  });
+}
+
+
--- source/js/slick-slider.js
+++ source/js/slick-slider.js
@@ -1,39 +1,115 @@
-$(document).ready(function () {
-  $('.slick').slick({
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    variableWidth: true,
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 1
+if (document.querySelector('.slick')) {
+  $(document).ready(function () {
+    $('.slick').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      variableWidth: true,
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 479,
+          settings: {
+            arrows: false,
+            dots: true,
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
         }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 479,
-        settings: {
-          arrows: false,
-          dots: true,
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+      ]
+    });
   });
-});
+}
+
+if (document.querySelector('.slick__appointments')) {
+  $(document).ready(function () {
+    $('.slick__appointments').slick({
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      variableWidth: true,
+      responsive: [
+        {
+          breakpoint: 1919,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            arrows: false,
+            dots: true,
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 479,
+          settings: {
+            arrows: false,
+            dots: true,
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+  });
+}
+
+if (document.querySelector('.all-services__slider')) {
+  $(document).ready(function () {
+    $('.all-services__slider').slick({
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      variableWidth: true,
+      responsive: [
+        {
+          breakpoint: 1919,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            arrows: false,
+            dots: true,
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 479,
+          settings: {
+            arrows: false,
+            dots: true,
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+  });
+}
