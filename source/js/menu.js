94eb73c6e099487c0e89c91aef040a0a2951cbb8
ninja_frontend <stifffffler102@gmail.com>
2023-03-10 18:02:23 +0300

    create build/blog.html and fix another

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,11 +1,26 @@
 const buttonMenu = document.querySelector('.menu__button');
 const menu = document.querySelector('.menu__list');
+const body = document.querySelector('.page-body');
 
+const toggleMenu = () => {
+  buttonMenu.classList.toggle('menu__button--active');
+  menu.classList.toggle('menu__list--active');
+  body.classList.toggle('blur');
+}
 
-const buttonMenuClicker = () => buttonMenu.addEventListener('click', () => {
-  buttonMenu.classList.toggle('menu__button--open');
-  buttonMenu.classList.toggle('menu__button--close');
-  menu.classList.toggle('menu__list--open');
+buttonMenu.addEventListener('click', e => {
+  e.stopPropagation();
+  toggleMenu();
 });
 
-buttonMenuClicker();
+document.addEventListener('click', e => {
+  let target = e.target;
+  let itsMenu = target == menu || menu.contains(target);
+  let itsButton = target == buttonMenu;
+  let menuIsActive = menu.classList.contains('menu__list--active');
+
+  if (!itsMenu && !itsButton && menuIsActive) {
+    e.preventDefault();
+    toggleMenu();
+  }
+});
